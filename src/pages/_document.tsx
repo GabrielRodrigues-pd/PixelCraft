2cbe329c4229dce878e4d09c122dd7fc53e2b10e
gabriel <gabriel.r.lacerda@hotmail.com>
2024-01-10 22:20:49 -0300

    stitche rode do lado do servidor

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { getCssText } from "../styles/stitches.config";
 
 export default function Document() {
   return (
@@ -13,6 +14,11 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@400;500;700&family=Space+Grotesk:wght@400;500;700&display=swap"
           rel="stylesheet"
+        />
+        {/* Permite que o css do stitche rode do lado do servidor */}
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
         />
       </Head>
       <body>
