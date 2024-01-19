7ff6a36cdccdaa027cad311d138e38def018804e
gabriel <gabriel.r.lacerda@hotmail.com>
2024-01-19 13:39:04 -0300

    html feito/90%

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, CardContact, Box } from "./styles";
+import { Container, Flame, BoxContact, BoxEmail } from "./styles";
 import ButtonV2 from "../Buttons/ButtonV2";
 import ButtonMsg from "../Buttons/ButtonMsg";
 import IconEmail from "@/assets/email.svg";
@@ -8,24 +8,35 @@ import Image from "next/image";
 export default function Contact() {
   return (
     <Container>
-      <h3>
-        Ainda tem alguma dúvida? <br /> Entre em contato
-      </h3>
-      <Box>
-        <CardContact>
+      <Flame>
+        <h2>
+          Vamos iniciar um <br /> Projeto?
+        </h2>
+        <p>
+          Você tem uma ideia inovadora? Está em busca de parcerias para
+          transformá-la em realidade? Então, vamos criar um projeto juntos!
+        </p>
+        <ButtonV2
+          title="Iniciar um projeto"
+          altDisc="Botão para iniciar projeto"
+        />
+      </Flame>
+      <BoxContact>
+        <h3>
+          Ainda tem alguma dúvida? <br /> Entre em contato
+        </h3>
+        <BoxEmail>
           <Image src={IconEmail} alt="" />
           <div>
-            <h3>E-mail</h3>
+            <h4>E-mail</h4>
             <p>Envie sua dúvida por e-mail</p>
             <a href="mailto:studiopixelcraft@contato.com">
               studiopixelcraft@contato.com
             </a>
           </div>
-        </CardContact>
-        <h2>Vamos criar um pojeto?</h2>
-        <ButtonV2 title="Iniciar um projeto" altDisc="Botão iniciar projeto" />
-      </Box>
-      <ButtonMsg />
+        </BoxEmail>
+        <ButtonMsg />
+      </BoxContact>
     </Container>
   );
 }
--- src/components/Contact/styles.ts
+++ src/components/Contact/styles.ts
@@ -1,67 +1,85 @@
 import { styled } from "@/styles/stitches.config";
 
 export const Container = styled("section", {
-  "& h3": {
-    color: "$white",
-    fontFamily: "$Space",
-    fontSize: "1.8rem",
-    fontWeight: 700,
-  },
-});
-
-export const CardContact = styled("div", {
-  borderRadius: ".8rem",
-  border: "2px solid $gray_btn",
-  padding: "2rem 0 2rem 3rem",
   display: "flex",
-  alignItems: "flex-start",
-  gap: "1.6rem",
-  width: "39.6rem",
-
-  "& h3": {
-    color: "$blue_100",
-    fontFamily: "$Readex",
-    fontSize: "1.6rem",
-    fontStyle: "normal",
-    fontWeight: 500,
-    lineHeight: "normal",
-    letterSpacing: "0.8px",
-    paddingBottom: ".3rem",
-  },
-  "& p": {
-    color: "$gray_span",
-    fontFamily: "$Readex",
-    fontSize: "1.6rem",
-    fontStyle: "normal",
-    fontWeight: 400,
-    lineHeight: "normal",
-    letterSpacing: "0.8px",
-    paddingBottom: "1.8rem",
-  },
-
-  "& a": {
-    color: "$blue_700",
-    fontFamily: "$Readex",
-    fontSize: "1.6rem",
-    fontStyle: "normal",
-    fontWeight: 400,
-    lineHeight: "normal",
-    letterSpacing: "0.8px",
-    paddingBottom: "2rem",
-  },
-});
-
-export const Box = styled("div", {
-  display: "flex",
-  alignItems: "center",
   justifyContent: "space-between",
-  margin: "3.6rem 0rem .8rem 0rem",
+  alignItems: "center",
+  height: "60vh",
+});
+
+export const Flame = styled("div", {
+  maxWidth: "56.8rem",
 
   "& h2": {
     color: "$white",
     fontFamily: "$Readex",
     fontSize: "3.2rem",
-    fontWeight: 700,
+    fontStyle: "normal",
+    fontWeight: 600,
+    lineHeight: "130%" /* 41.6px */,
     letterSpacing: "1.6px",
+  },
+  "& p": {
+    color: "$gray_300",
+    fontFamily: "$Readex",
+    fontSize: "1.8rem",
+    fontStyle: "normal",
+    fontWeight: 400,
+    lineHeight: "150%" /* 27px */,
+    padding: "1.6rem 0 0 0",
+  },
+});
+
+export const BoxContact = styled("div", {
+  "& h3": {
+    color: "$white",
+    fontFamily: "$Space",
+    fontSize: "1.8rem",
+    fontStyle: "normal",
+    fontWeight: 700,
+    lineHeight: "normal",
+  },
+});
+
+export const BoxEmail = styled("div", {
+  borderRadius: ".8rem",
+  border: "2px solid $gray_btn",
+  margin: "3.4rem 0 1.6rem 0",
+  padding: "2.2rem 0 2.2rem 3.7rem",
+
+  display: "flex",
+  alignItems: "flex-start",
+  gap: "1.6rem",
+
+  "& h4": {
+    color: "$blue_100",
+    fontFamily: "$Space", // Fira Sans
+    fontSize: "1.6rem",
+    fontStyle: "normal",
+    fontWeight: 500,
+    lineHeight: "normal",
+    letterSpacing: "0.8px",
+  },
+
+  "& p": {
+    color: "$gray_span",
+    fontFamily: "$Space", // Fira Sans
+    fontSize: "1.6rem",
+    fontStyle: "normal",
+    fontWeight: 400,
+    lineHeight: "normal",
+    letterSpacing: "0.8px",
+    padding: ".5rem 0 2rem 0",
+  },
+
+  "& a": {
+    color: "$blue_700",
+    fontFamily: "$Space", // Fira Sans
+    fontSize: "1.4rem",
+    fontStyle: "normal",
+    fontWeight: 700,
+    lineHeight: "normal",
+    letterSpacing: "0.7px",
+    textDecoration: "none",
   },
 });
